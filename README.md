304ef9c507fa75ed4a69f5e860da57ade4a74bde
Grimalkinn <okwuoasfelix634@gmail.com.com>
2024-10-15 22:16:12 +0300

    updated and added new methods

--- README.md
+++ README.md
@@ -1,21 +1,30 @@
 # Simle-Java-Syntax
-a java class that that simplifies java syntax like printing, input, file handling, type checks etc
+a java class that that simplifies core java syntax like printing, input, file handling, type checks etc
 
-Usage
+Usage - simply extend the <SimpleSyntax> class to get access to methods
 -----
 ```java
-public class Test extends SimpleSyntax {
+public class MyClass extends SimpleSyntax {
 
-    public void run() {
-        print("Hello, World!");
-        String age = input("Enter age: ");
-        age = num(age);
-        print(age + " " + type(age));
+    public static void main(String[] args) { // example in a static method
+        Test test = new Test();
+        Myclass.myMethod();
+
+        print(type(0));
     }
 
-  public static void main(String[] args) {
-        Test test = new Test();
-        test.run();
+
+    public void myMethod() { // example in a non-static method
+        print("Hello, World!");
+
+        String name = input("Enter name: "); 
+        int age = itr(input("Enter age: ")); 
+
+        print(age + " " + type(age));
+
+        Object[] objects = new Object[]{1,2,3};
+        objects = append(objects, 4);
+        print(objects);
     }
 
 }
--- SimpleSyntax.java
+++ SimpleSyntax.java
@@ -3,60 +3,85 @@ import java.util.*;
 
 public class SimpleSyntax {
     
-    public SimpleSyntax(){}
+    SimpleSyntax(){}
+
+    public static void main(String[] args) {
+        // test code here
+        Object[] objects = new Object[]{1,2,3};
+        objects = append(objects, 4);
+        print(objects);
+    }
 
     // simply print
-    public void print(int s) { System.out.println(s); }
-    public void print(char s) { System.out.println(s); }
-    public void print(byte s) { System.out.println(s); }
-    public void print(long s) { System.out.println(s); }
-    public void print(float s) { System.out.println(s); }
-    public void print(Object o) { System.out.println(o); }
-    public void print(double s) { System.out.println(s); }
-    public void print(String s) { System.out.println(s); }
-    public void print(boolean s) { System.out.println(s); }
+    public static void println(Object obj) {  if (obj != null) System.out.println(obj); else System.out.println(""); }
+    public static void print(Object obj) { if (obj != null) System.out.print(obj); else System.out.println(""); }
+
+    // simply print arrays
+    public static void print(Object obj[]) { try { System.out.print(Arrays.toString(obj));} catch(Exception ex) {;;} }
+    public static void println(Object obj[]) { try { System.out.println(Arrays.toString(obj));} catch(Exception ex) {;;} }
 
     // simply return object type as string
-    public String type(Object s){ return s.getClass().getSimpleName(); }
+    public static String type(Object obj){ return obj.getClass().getSimpleName(); }
 
     // simply input
-    public String input(){
-        Scanner scan = new Scanner(System.in);
-        String terminal_input = scan.nextLine();
-        scan.close();
-        return terminal_input;
+    public static String input(){
+        try (Scanner scan = new Scanner(System.in)){
+            String terminal_input = scan.nextLine();
+            scan.close();
+            return terminal_input;
+        }
     }
 
     // simply input with msg
-    public String input(String msg){
+    public static String input(String msg){
         System.out.print(msg);
         String terminal_input = input();
         return terminal_input;
     }
 
+
     // simply read a file
-    public String read(String filename, int lines) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename))){      
-            String line = "";
-            for (int i=0; i<lines-1; i++)
-                line = line.concat(reader.readLine() + "\n");
-            return line;
+    public static String read(String filename, int lines) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))){
+            String content = ""; String line = ""; String quanta = ""; 
+            int num = 0;
+
+            while ((line = reader.readLine()) != null) 
+                content = content.concat(line + "\n"); 
+                num++;
+            
+            if (lines > num || lines <= 0) return content;
+            else 
+                num = 0; 
+                while ((line = reader.readLine()) != null && num <= lines) 
+                    quanta = quanta.concat(line + "\n"); num++;
+                    num ++;
+
+            return quanta;
         }
     }
 
     // simply write or append a file
-    public void write(String filename, boolean append, String msg) throws IOException {
-        try (BufferedWriter buffer = new BufferedWriter(new FileWriter(filename, append))){
-            buffer.write(msg);
+    public static void write(String filename, boolean append, String msg) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, append))){
+            writer.write(msg);
         }
     }
 
     // simply convert to string
-    public String str(Object obj){ return String.valueOf(obj); }
+    public static String str(Object obj){ return String.valueOf(obj); }
+
     // simply convert to int
-    public int num(Object obj){ return (int) Integer.parseInt(str(obj).trim()); }
+    public static int itr(Object obj){ return (int) Integer.parseInt(str(obj).trim()); }
 
-
+    // simply append to any array of type Object
+    public static Object[] append(Object[] prev, Object data) {
+        Object[] newArray = new Object[prev.length +1];
+        for (int i = 0; i < prev.length; i++) newArray[i] = prev[i];
+        newArray[newArray.length-1] = data;
+        return newArray;
+    }
+ 
 } 
 
 // END
